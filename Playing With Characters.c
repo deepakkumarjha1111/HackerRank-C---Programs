b7b9dabcc78e123f94fbb7b1892f865f193dc033
Arjun Naik <55954515+arjunnaik@users.noreply.github.com>
2021-06-09 00:13:36 +0530

    Create Playing With Characters.c

---
+++ Playing With Characters.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+ char ch[100];
+ char s[100];
+ char sen[100];
+ scanf("%c",&ch);
+ printf("%s\n",ch);
+ scanf("%s",&s);
+ printf("%s\n",s);
+ scanf("\n");
+ scanf("%[^\n]%*c", sen);
+ printf("%s",sen);
+ 
+    return 0;
+}
